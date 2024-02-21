6ae6c70194fb481abfa0389e3f14fbfd1f474ce8
Teodor <teodorvoicu87@gmail.com>
2024-02-21 19:52:33 +0200

    customize header

---
+++ src/customizations/volto/components/theme/Header/Header.jsx
@@ -0,0 +1,200 @@
+/**
+ * Header component.
+ * @module components/theme/Header/Header
+ */
+
+import React from 'react';
+import { Dropdown, Image } from 'semantic-ui-react';
+import { connect, useDispatch, useSelector } from 'react-redux';
+
+import { withRouter } from 'react-router-dom';
+import { UniversalLink } from '@plone/volto/components';
+import {
+  getBaseUrl,
+  hasApiExpander,
+  // flattenToAppURL,
+  BodyClass,
+} from '@plone/volto/helpers';
+import { getNavigation } from '@plone/volto/actions';
+import { Header, Logo } from '@eeacms/volto-eea-design-system/ui';
+import { usePrevious } from '@eeacms/volto-eea-design-system/helpers';
+// import { find } from 'lodash';
+// import globeIcon from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/global-line.svg';
+import eeaFlag from '@eeacms/volto-eea-design-system/../theme/themes/eea/assets/images/Header/eea.png';
+
+import config from '@plone/volto/registry';
+import { compose } from 'recompose';
+
+import cx from 'classnames';
+
+function removeTrailingSlash(path) {
+  return path.replace(/\/+$/, '');
+}
+
+/**
+ * EEA Specific Header component.
+ */
+const EEAHeader = ({ pathname, token, items, history, subsite }) => {
+  const router_pathname = useSelector((state) => {
+    return removeTrailingSlash(state.router?.location?.pathname) || '';
+  });
+
+  // const isSubsite = subsite?.['@type'] === 'Subsite';
+
+  const isHomePageInverse = useSelector((state) => {
+    const layout = state.content?.data?.layout;
+    const has_home_layout =
+      layout === 'homepage_inverse_view' ||
+      (__CLIENT__ && document.body.classList.contains('homepage-inverse'));
+    return has_home_layout;
+  });
+
+  const { eea } = config.settings;
+  const headerOpts = eea.headerOpts || {};
+  const headerSearchBox = eea.headerSearchBox || [];
+  const width = useSelector((state) => state.screen?.width);
+  const dispatch = useDispatch();
+  const previousToken = usePrevious(token);
+
+  React.useEffect(() => {
+    const { settings } = config;
+    const base_url = getBaseUrl(pathname);
+    if (!hasApiExpander('navigation', base_url)) {
+      dispatch(getNavigation(base_url, settings.navDepth));
+    }
+  }, [pathname, dispatch]);
+
+  React.useEffect(() => {
+    if (token !== previousToken) {
+      const { settings } = config;
+      const base = getBaseUrl(pathname);
+      if (!hasApiExpander('navigation', base)) {
+        dispatch(getNavigation(base, settings.navDepth));
+      }
+    }
+  }, [token, dispatch, pathname, previousToken]);
+
+  return (
+    <Header menuItems={items}>
+      {isHomePageInverse && <BodyClass className="homepage" />}
+      <Header.TopHeader>
+        <Header.TopItem className="official-union">
+          <Image src={eeaFlag} alt="eea flag"></Image>
+          <Header.TopDropdownMenu
+            text="An official website of the European Union | How do you know?"
+            tabletText="EEA information systems"
+            mobileText=" "
+            icon="chevron down"
+            aria-label="dropdown"
+            className=""
+            viewportWidth={width}
+          >
+            {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions */}
+            <div
+              className="content"
+              onClick={(evt) => evt.stopPropagation()}
+              onKeyDown={(evt) => evt.stopPropagation()}
+            >
+              <p>
+                All official European Union website addresses are in the{' '}
+                <b>europa.eu</b> domain.
+              </p>
+              <a
+                href="https://europa.eu/european-union/contact/institutions-bodies_en"
+                target="_blank"
+                rel="noreferrer"
+                onKeyDown={(evt) => evt.stopPropagation()}
+              >
+                See all EU institutions and bodies
+              </a>
+            </div>
+          </Header.TopDropdownMenu>
+        </Header.TopItem>
+
+        {!!headerOpts.partnerLinks && (
+          <Header.TopItem>
+            <Header.TopDropdownMenu
+              id="theme-sites"
+              text={headerOpts.partnerLinks.title}
+              viewportWidth={width}
+            >
+              <div className="wrapper">
+                {headerOpts.partnerLinks.links.map((item, index) => (
+                  <Dropdown.Item key={index}>
+                    <a
+                      href={item.href}
+                      className="site"
+                      target="_blank"
+                      rel="noreferrer"
+                      onKeyDown={(evt) => evt.stopPropagation()}
+                    >
+                      {item.title}
+                    </a>
+                  </Dropdown.Item>
+                ))}
+              </div>
+            </Header.TopDropdownMenu>
+          </Header.TopItem>
+        )}
+      </Header.TopHeader>
+      <Header.Main
+        pathname={pathname}
+        headerSearchBox={headerSearchBox}
+        logo={
+          <div>
+            {headerOpts.logo.map((logo) => (
+              <Logo
+                src={logo}
+                title={eea.websiteTitle}
+                alt={eea.organisationName}
+                url={eea.logoTargetUrl}
+                key={logo}
+              />
+            ))}
+          </div>
+        }
+        menuItems={items}
+        renderGlobalMenuItem={(item, { onClick }) =>
+          item.title !== 'Home' && (
+            <a
+              href={item.url || '/'}
+              title={item.title}
+              onClick={(e) => {
+                e.preventDefault();
+                onClick(e, item);
+              }}
+            >
+              {item.title}
+            </a>
+          )
+        }
+        renderMenuItem={(item, options, props) => (
+          <UniversalLink
+            href={item.url || '/'}
+            title={item.nav_title || item.title}
+            {...(options || {})}
+            className={cx(options?.className, {
+              active: item.url === router_pathname,
+            })}
+          >
+            {props?.iconPosition !== 'right' && props?.children}
+            <span>{item.nav_title || item.title}</span>
+            {props?.iconPosition === 'right' && props?.children}
+          </UniversalLink>
+        )}
+      ></Header.Main>
+    </Header>
+  );
+};
+
+export default compose(
+  withRouter,
+  connect(
+    (state) => ({
+      token: state.userSession.token,
+      items: state.navigation.items,
+      subsite: state.content.data?.['@components']?.subsite,
+    }),
+    { getNavigation },
+  ),
+)(EEAHeader);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import { Sitemap } from '@plone/volto/components';
+import copernicusLogo from '@eeacms/volto-insitu-policy/../theme/assets/images/Header/copernicus_eu_logo.svg';
+import insituLogo from '@eeacms/volto-insitu-policy/../theme/assets/images/Header/in_situ_logo.svg';
 
 const applyConfig = (config) => {
+  //EEA customizations
+  config.settings.eea = {
+    ...(config.settings.eea || {}),
+    headerOpts: {
+      ...(config.settings.eea?.headerOpts || {}),
+      logo: [copernicusLogo, insituLogo],
+    },
+  };
+
+  //Routes
   config.addonRoutes = [
     {
       path: '/sitemap',
