0a0a9d145bb6371dd4e2025706893936eb159618
Teodor <teodorvoicu87@gmail.com>
2024-02-21 11:39:46 +0200

    Add sitemap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import { Sitemap } from '@plone/volto/components';
+
 const applyConfig = (config) => {
+  config.addonRoutes = [
+    {
+      path: `/(${config.settings?.supportedLanguages.join('|')})/sitemap`,
+      component: Sitemap,
+    },
+  ];
   return config;
 };
 
