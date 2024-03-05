648883926febf4f22167f16622f6b90c284d0623
Teodor <teodorvoicu87@gmail.com>
2024-03-05 12:22:27 +0200

    change subfooter structure

--- src/customizations/@eeacms/volto-eea-design-system/ui/Footer/SubFooter.jsx
+++ src/customizations/@eeacms/volto-eea-design-system/ui/Footer/SubFooter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid, Image } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 
 const SubFooter = (props) => {
   if (props.children) {
@@ -12,44 +12,11 @@ const SubFooter = (props) => {
       <div className="subfooter-logo-container">
         {props.managedBy &&
           props.managedBy.map((manager, index) => (
-            <div className="item">
-              <div className={manager.className}>
-                <a href={manager.url}>
-                  <Image
-                    src={manager.src}
-                    alt={manager.alt}
-                    loading="lazy"
-                  ></Image>
-                </a>
-              </div>
-            </div>
-            // </Grid.Column>
+            <a href={manager.url}>
+              <Image src={manager.src} alt={manager.alt} loading="lazy"></Image>
+            </a>
           ))}
       </div>
-
-      <Grid className="mobile only">
-        {props.managedBy &&
-          props.managedBy.map((manager, index) => (
-            <Grid.Column
-              mobile={manager.columnSize.mobile}
-              tablet={manager.columnSize.tablet}
-              computer={manager.columnSize.computer}
-              key={index}
-            >
-              <div className="item">
-                <div className={manager.className}>
-                  <a href={manager.url}>
-                    <Image
-                      src={manager.src}
-                      alt={manager.alt}
-                      loading="lazy"
-                    ></Image>
-                  </a>
-                </div>
-              </div>
-            </Grid.Column>
-          ))}
-      </Grid>
     </div>
   );
 };
