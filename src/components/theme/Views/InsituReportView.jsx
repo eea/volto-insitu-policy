ab334eaadf8b5e6109997c6e5e96bd2c886d5519
GhitaB <ghitabizau27@gmail.com>
2024-04-16 17:07:09 +0300

    Refs #268580 - Simplify view.

--- src/components/theme/Views/InsituReportView.jsx
+++ src/components/theme/Views/InsituReportView.jsx
@@ -40,16 +40,6 @@ function InsituReportView(props) {
       <BannerTitle content={content} />
       <div className="ui container">
         <p className="documentDescription eea callout">{content.description}</p>
-        {!!content.image && (
-          <div className="lead-img">
-            <img
-              className="main-img"
-              src={content.image?.download}
-              alt={content.title}
-            />
-            <p className="main-img-caption">{content.image_caption}</p>
-          </div>
-        )}
         {!!content.file?.download && (
           <div className="file-download">
             <a href={content.file.download}>{content.file.filename}</a>
