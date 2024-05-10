489c87edca95cbe5fac323a55b7b895c76c04333
GhitaB <ghitabizau27@gmail.com>
2024-05-10 10:50:29 +0300

    Refs #268585 - Update data providers list widget to use new serializer.

--- src/components/theme/Widgets/DataProviderListWidget.jsx
+++ src/components/theme/Widgets/DataProviderListWidget.jsx
@@ -1,13 +1,35 @@
 function DataProviderListWidget(props) {
   const { value } = props;
+
+  /*
+  Example of data providers list:
+    [{
+    "acronym": "AAA",
+    "countries": [
+      {
+        "code": "IT",
+        "name": "Italy"
+      }
+    ],
+    "id": 1,
+    "is_network": false,
+    "link": "https://test.test/1/",
+    "members": [],
+    "name": "Aaaa AAAaa AAAAA"
+  }, ...]
+  */
+
   return (
-    <ul>
-      {value.map((item, index) => (
-        <li key={index}>
-          <a href="https://insitu.copernicus.eu/">{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <div className="data-providers">
+      <h4>Data providers</h4>
+      <ul>
+        {value.map((item, index) => (
+          <li key={index}>
+            <a href={item.link}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
