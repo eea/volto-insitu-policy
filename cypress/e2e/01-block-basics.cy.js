28c0ab2377fc2ec60fc4222fe9849bf7c34fa7ca
Teodor <teodorvoicu87@gmail.com>
2024-05-23 12:31:13 +0300

    cypress test

--- cypress/e2e/01-block-basics.cy.js
+++ cypress/e2e/01-block-basics.cy.js
@@ -5,7 +5,7 @@ describe('Blocks Tests', () => {
   afterEach(slateAfterEach);
 
   it('Add Block: Empty', () => {
-    cy.get('iframe').invoke('css', 'display', 'none'); //hide element covering other elements
+    // cy.get('iframe').invoke('css', 'display', 'none');
     // Change page title
     cy.clearSlateTitle();
     cy.getSlateTitle().type('My Add-on Page');
