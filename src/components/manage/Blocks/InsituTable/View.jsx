df3a060dd518dc0062c05c7322e81400eb66cdbb
Teodor <teodorvoicu87@gmail.com>
2024-05-27 21:15:24 +0300

    update ui for search component

--- src/components/manage/Blocks/InsituTable/View.jsx
+++ src/components/manage/Blocks/InsituTable/View.jsx
@@ -7,7 +7,6 @@ import {
 } from '@tanstack/react-table';
 import './styles.less';
 import { useSelector } from 'react-redux';
-// import { data_providers } from '@eeacms/volto-datablocks/src/reducers';
 
 const LinkCell = ({ cell }) => {
   return <a href={cell.getValue()}>{cell.getValue()}</a>;
@@ -135,30 +134,30 @@ const DataProvidersTable = (props) => {
   return (
     <>
       <div className="search-container">
-        <div className="search-icon-container">
-          <input
-            type="text"
-            value={filtering}
-            onChange={(e) => setFiltering(e.target.value)}
-            placeholder="Search..."
-            className="search-input"
-          />
-          {/* <div className="search-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
-              viewBox="0 0 36 36"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7,16 C7,11.038 11.037,7 16,7 C20.963,7 25,11.038 25,16 C25,20.962 20.963,25 16,25 C11.037,25 7,20.962 7,16 L7,16 Z M32.707,31.293 L24.448,23.034 C26.039,21.125 27,18.673 27,16 C27,9.935 22.065,5 16,5 C9.935,5 5,9.935 5,16 C5,22.065 9.935,27 16,27 C18.673,27 21.125,26.039 23.034,24.448 L31.293,32.707 L32.707,31.293 Z"
-              />
-            </svg>
-          </div> */}
+        <input
+          type="text"
+          value={filtering}
+          onChange={(e) => setFiltering(e.target.value)}
+          placeholder="Start typing to filter by any column value"
+          className="search-input"
+        />
+        <div className="search-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="18"
+            height="18"
+            viewBox="0 0 36 36"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7,16 C7,11.038 11.037,7 16,7 C20.963,7 25,11.038 25,16 C25,20.962 20.963,25 16,25 C11.037,25 7,20.962 7,16 L7,16 Z M32.707,31.293 L24.448,23.034 C26.039,21.125 27,18.673 27,16 C27,9.935 22.065,5 16,5 C9.935,5 5,9.935 5,16 C5,22.065 9.935,27 16,27 C18.673,27 21.125,26.039 23.034,24.448 L31.293,32.707 L32.707,31.293 Z"
+              fill="none"
+              stroke="#783f34"
+              stroke-width="2"
+            />
+          </svg>
         </div>
       </div>
-      <hr />
       <table>
         <thead>
           {tableInstance.getHeaderGroups().map((headerGroup) => (
